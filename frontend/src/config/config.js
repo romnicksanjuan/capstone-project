177dceb7dd6c83aa591919c082ecf475d31fce4c
romnick <romnicksanjuan9@gmail.com>
2025-02-19 15:29:41 +0800

    news

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,6 +1,7 @@
 const DOMAIN = {
     local_domain: 'http://localhost:3001',
+    server_domain: 'https://capstone-project-server-seven.vercel.app'
 
 }
 
-export default DOMAIN.local_domain
+export default DOMAIN.server_domain
