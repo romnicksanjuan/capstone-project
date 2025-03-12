cbbe7ba57523723e5dd5c3dc51d0c8051dcda86e
romnick <romnicksanjuan9@gmail.com>
2025-03-12 18:16:15 +0800

    gg

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -4,10 +4,10 @@ import logo from '../images/ctc logo.png'
 const Topbar = () => {
     return (
 
-        <div style={{ display: 'flex', alignItems: 'center', gap: "10px", marginBottom: "40px" }}>
+        <div style={{ display: 'flex', alignItems: 'center', gap: "10px", marginBottom: "35px" }}>
             <img src={logo} style={{ width: "110px", height: 'auto' }} />
 
-            <div style={{ width: "50%" }}>
+            <div style={{ width: "70%" }}>
                 <h2 style={{ fontSize: '25px', textAlign: 'center', color: 'white' }}>Property and Merchandise Hub Management
                     System with QR Code Tracking of Ceguera Technological Colleges</h2>
             </div>
